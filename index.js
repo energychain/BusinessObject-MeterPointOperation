c6ac1dead5e63130345d98106733fe705dcbf05a
Thorsten Zoerner <me@thorsten-zoerner.com>
2017-09-14 00:45:15 +0200

    Added Vorpal Dependency

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+#!/usr/bin/env node
+
+const vorpal = require('vorpal')();
+var interactive = vorpal.parse(process.argv, {use: 'minimist'})._ === undefined;
+
 /* StromDAO Business Object: MeterPoint Operation
  * =========================================
  * Meter Point Operator handling for StromDAO Energy Blockchain.
@@ -5,8 +10,8 @@
  * @author Thorsten Zoerner thorsten.zoerner@stromdao.de 
  * 
  * Usage: 
- *    stbo-mpo store YOUR_METERPOINT_ID READING
- *    stbo-mpo retrieve YOUR_METERPOINT_ID
+ *    stromdao-mp store YOUR_METERPOINT_ID READING
+ *    stromdao-mp retrieve YOUR_METERPOINT_ID
  * 
  * This script will automatically assign a unique energy blockchain address for 
  * your meterpoint. 
@@ -17,6 +22,50 @@
  
 var StromDAOBO = require("stromdao-businessobject");    
 
+
+vorpal
+  .command('store <meter_point_id> <reading>')    
+  .description("Stores Meter Point Reading for given external Meter Point ID.") 
+  .action(function (args, callback) {	 
+	var node = new StromDAOBO.Node({extid:args.meter_point_id,testMode:true});	
+	node.mpr().then( function(mpo) {
+			mpo.storeReading(args.reading).then( function(tx_result) {	
+					vorpal.log("TX:",tx_result);
+					callback();
+			});
+	});	
+});	
+
+vorpal
+  .command('retrieve <meter_point_id>')    
+  .description("Retrieves Meter Point Reading for given external Meter Point ID.") 
+  .action(function (args, callback) {	 
+	var node = new StromDAOBO.Node({extid:args.meter_point_id,testMode:true});	
+	node.mpr().then( function(mpo) {
+			mpo.readings(node.wallet.address).then( function(tx_result) {								
+				vorpal.log("Time:",new Date(tx_result.time.toString()*1000).toLocaleString());
+				vorpal.log("Reading:",tx_result.power.toString());
+				callback();									
+			});			
+	});	
+});	
+
+
+if (interactive) {
+    vorpal
+        .delimiter('stromdao-mp $')
+        .show()
+} else {
+    // argv is mutated by the first call to parse.
+    process.argv.unshift('')
+    process.argv.unshift('')
+    vorpal
+        .delimiter('')
+        .parse(process.argv)
+}
+
+
+/*
 var userArgs = process.argv.slice(2);
 
 if(userArgs.length<1) {
@@ -35,11 +84,7 @@ if(userArgs[0]=="store") {
 		console.log("ERR: Missing Reading");
 		process.exit(1);
 	}
-    node.mpo().then( function(mpo) {
-							mpo.storeReading(userArgs[2]).then( function(tx_result) {	
-									console.log("Tx",tx_result,userArgs[2]);
-							});
-						});
+    
 }	
 if(userArgs[0]=="retrieve") {
 	node.mpo().then( function(mpo) {
@@ -55,3 +100,4 @@ if(userArgs[0]=="blockchain") {
 							});
 						});
 }	
+*/
