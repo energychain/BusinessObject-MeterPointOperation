d39e8a2f64a20a55318e25255084d46cac4bc942
Thorsten Zoerner <me@thorsten-zoerner.com>
2017-09-14 01:02:20 +0200

    Added ABI Location for Web-Retrieval

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ vorpal
   .command('store <meter_point_id> <reading>')    
   .description("Stores Meter Point Reading for given external Meter Point ID.") 
   .action(function (args, callback) {	 
-	var node = new StromDAOBO.Node({extid:args.meter_point_id,testMode:true});	
+	var node = new StromDAOBO.Node({extid:args.meter_point_id,testMode:true,abilocation:"https://cdn.rawgit.com/energychain/StromDAO-BusinessObject/master/smart_contracts/"});	
 	node.mpr().then( function(mpo) {
 			mpo.storeReading(args.reading).then( function(tx_result) {	
 					vorpal.log("TX:",tx_result);
@@ -40,7 +40,7 @@ vorpal
   .command('retrieve <meter_point_id>')    
   .description("Retrieves Meter Point Reading for given external Meter Point ID.") 
   .action(function (args, callback) {	 
-	var node = new StromDAOBO.Node({extid:args.meter_point_id,testMode:true});	
+	var node = new StromDAOBO.Node({extid:args.meter_point_id,testMode:true,abilocation:"https://cdn.rawgit.com/energychain/StromDAO-BusinessObject/master/smart_contracts/"});	
 	node.mpr().then( function(mpo) {
 			mpo.readings(node.wallet.address).then( function(tx_result) {								
 				vorpal.log("Time:",new Date(tx_result.time.toString()*1000).toLocaleString());
