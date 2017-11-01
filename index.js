71fa23ac666834891626c4319e5bd1b91dd4951f
Thorsten Zoerner <me@thorsten-zoerner.com>
2017-11-01 13:19:05 +0100

    Force setting of role 41 (Parent BLG) in webuser creation

--- index.js
+++ index.js
@@ -804,48 +804,52 @@ vorpal
   .action(function (args, callback) {	
 	  ensure_balancing(args,callback,function() {		  
 		var account_obj=new StromDAOBO.Account(args.options.username,args.options.password);
-		var node = new StromDAOBO.Node({external_id:args.meter_point_id,testMode:true});	
-		account_obj.wallet().then(function(wallet) {
-				account_obj.encrypt(node.wallet.privateKey).then(function(enc) {
-					node.stringstoragefactory().then(function(ssf)  {						
-						ssf.build(enc).then(function(ss) {
-							var node = new StromDAOBO.Node({external_id:args.options.username,privateKey:wallet.privateKey,testMode:true});	
-							node.roleLookup().then(function(rl) {
-									rl.setRelation(222,ss).then(function(tx) {
-										vorpal.log("Webuser created",tx);									
-										// Dump Profile
-										if(typeof args.options.file != "undefined") {
-												
-													var storage = require("node-persist");
-													var fs = require("fs");
-													storage.initSync();
-													values=storage.keys();
-													var tmp = {};
-													
-													for (var k in values){
-														if (values.hasOwnProperty(k)) {			
-															if(values[k].substr(0,"address_".length)=="address_") {				
-																tmp[""+values[k].toLowerCase()]=storage.getItemSync(""+values[k]);																	
-															}
-															if(values[k].substr(0,"name_".length)=="name_") {				
-																tmp[""+values[k].toLowerCase()]=storage.getItemSync(""+values[k]);																	
-															}
-														}
-													}	
-													fs.writeFile(args.options.file, JSON.stringify(tmp), 'utf8', function() {
-														vorpal.log("Profile File written");
-														callback(); 
-														});	
-										} else {										
-										callback();
-										}
+		var node = new StromDAOBO.Node({external_id:args.meter_point_id,testMode:true});
+		node.roleLookup().then(function(rl) {
+				rl.setRelation(41,global.smart_contract_stromkonto).then(function(x) {									
+				account_obj.wallet().then(function(wallet) {
+						account_obj.encrypt(node.wallet.privateKey).then(function(enc) {
+							node.stringstoragefactory().then(function(ssf)  {						
+								ssf.build(enc).then(function(ss) {
+									var node = new StromDAOBO.Node({external_id:args.options.username,privateKey:wallet.privateKey,testMode:true});	
+									node.roleLookup().then(function(rl) {
+											rl.setRelation(222,ss).then(function(tx) {
+												vorpal.log("Webuser created",tx);									
+												// Dump Profile
+												if(typeof args.options.file != "undefined") {
+														
+															var storage = require("node-persist");
+															var fs = require("fs");
+															storage.initSync();
+															values=storage.keys();
+															var tmp = {};
+															
+															for (var k in values){
+																if (values.hasOwnProperty(k)) {			
+																	if(values[k].substr(0,"address_".length)=="address_") {				
+																		tmp[""+values[k].toLowerCase()]=storage.getItemSync(""+values[k]);																	
+																	}
+																	if(values[k].substr(0,"name_".length)=="name_") {				
+																		tmp[""+values[k].toLowerCase()]=storage.getItemSync(""+values[k]);																	
+																	}
+																}
+															}	
+															fs.writeFile(args.options.file, JSON.stringify(tmp), 'utf8', function() {
+																vorpal.log("Profile File written");
+																callback(); 
+																});	
+												} else {										
+												callback();
+												}
+											});
 									});
-							});
+								});
+							});							
 						});
-					});							
-				});
-			});	
-		});	  			
+					});	
+				});	
+			}):			
+		});				  			
 	});    
 
 vorpal
